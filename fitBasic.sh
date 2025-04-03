98394475b18a269fb95520cafd04b48bda04e721
Kamil Laurent <klaurent@jefferson.pv.infn.it>
2025-04-03 19:43:41 +0200

    first commit

--- fitBasic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=BasicNNPDF
-
-for i in {1..10}
-do
-  n3fit Basic_runcard.yml $i
-done
--- job_comparefits.sh
+++ job_comparefits.sh
@@ -3,5 +3,5 @@
 
 # Your script goes here
 conda activate nnpdf
-vp-comparefits nnpdf40-like 240701-02-rs-nnpdf40-baseline --title "test_comparison_1" --author "Kamil Laurent" --keywords "NNPDF comparison 1"
+vp-comparefits 240701-02-rs-nnpdf40-baseline 240701-02-rs-nnpdf40-baseline --title "test_comparison_1" --author "Kamil Laurent" --keywords "NNPDF comparison 1"
 
