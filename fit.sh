01b99f256dd614eb841b4e3be73745f9984cda28
Kamil Laurent <klaurent@jefferson.pv.infn.it>
2025-04-04 11:20:48 +0200

    Remove tracked .sh files now ignored

--- fit.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..10}
-do
-  n3fit Basic_runcard.yml $i
-done
-
--- job_nn40fit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=nnpdf40
-
-# Your script goes here
-conda activate nnpdf
-for i in {11..20}
-do
-  n3fit nnpdf40-like.yml $i
-done
